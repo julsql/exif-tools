66fa27e6b8aba436362616a32da7c5412a031c21
Jul SQL <julsql1@gmail.com>
2025-04-07 22:31:22 +0200

    feat: App icon

--- editor/editor_app.py
+++ editor/editor_app.py
@@ -1,5 +1,8 @@
-
+from tkinter import PhotoImage
 
 class ExifEditorApp:
     def __init__(self, root):
         self.root = root
+        icon = PhotoImage(file="./assets/icon.png")
+
+        root.iconphoto(True, icon)
