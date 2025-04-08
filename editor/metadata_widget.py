28c2ed446897e6ceabac6111f535f730533ae3fb
Jul SQL <julsql1@gmail.com>
2025-04-08 22:23:17 +0200

    feat: Metadata visualization panel

---
+++ editor/metadata_widget.py
@@ -0,0 +1,194 @@
+import os
+import tkinter as tk
+from datetime import datetime
+
+import piexif
+
+from editor.shared_data import ImageData, StyleData
+
+
+class MetadataWidget(tk.Frame):
+    def __init__(self, parent, image_data: ImageData, style_data: StyleData):
+        super().__init__(parent)
+        self.image_data = image_data
+        self.style_data = style_data
+        self.configure(bg=style_data.bg_color)
+
+        self.labels = [{"key": "nom", "title": "Nom", "disable": False},
+                       {"key": "format", "title": "Format", "disable": True},
+                       {"key": "poids", "title": "Poids", "disable": True},
+                       {"key": "dimensions", "title": "Dimensions", "disable": True},
+                       {"key": "date_creation", "title": "Date de création", "disable": False},
+                       {"key": "date_modification", "title": "Date de modification", "disable": True},
+                       {"key": "longitude", "title": "Longitude", "disable": False},
+                       {"key": "latitude", "title": "Latitude", "disable": False}]
+        self.entries = {}
+
+        for i, label_data in enumerate(self.labels):
+            label = tk.Label(self, text="{0} : ".format(label_data["title"]), bg=style_data.bg_tab_color, fg=style_data.font_color)
+            entry = tk.Entry(self,
+                             bg=style_data.bg_tab_color,
+                             fg=style_data.font_color,
+                             bd=1,
+                             highlightthickness=1,
+                             relief="flat",
+                             insertbackground=style_data.font_color
+                             )
+
+            label.grid(row=i, column=0, sticky="w", padx=5, pady=5)
+            entry.grid(row=i, column=1, sticky="ew", padx=5, pady=5)
+
+            entry.bind("<FocusIn>", self.on_focus_in)
+            entry.bind("<FocusOut>", self.on_focus_out)
+
+            if label_data["disable"]:
+                entry.config(state="disabled",
+                             disabledforeground=style_data.fg_disable,
+                             disabledbackground=style_data.bg_disable)
+            else:
+                btn_reset = tk.Button(self,
+                                      text="✖",
+                                      command=lambda local_key=label_data["key"], index=i: self.reset(local_key, index),
+                                      bg=style_data.bg_color,
+                                      fg=style_data.font_color,
+                                      highlightthickness=0,
+                                      highlightbackground=style_data.bg_color,
+                                      relief="flat")
+                btn_reset.grid(row=i, column=2, padx=5, pady=5)
+
+            self.entries[label_data["key"]] = entry
+
+        # Permet à l'entrée de s'étendre quand la fenêtre est redimensionnée
+        self.grid_columnconfigure(1, weight=1)
+
+        # Écouter l’événement
+        self.bind("<<ImageUpdated>>", self.update_metadata)
+        self.after(100, self.hook_imagewidget)
+
+    def on_focus_in(self, event):
+        """Changement de bordure quand l'entry reçoit le focus."""
+        event.widget.config(highlightbackground=self.style_data.select_color, highlightcolor=self.style_data.select_color)
+
+    def on_focus_out(self, event):
+        """Changement de bordure quand l'entry perd le focus."""
+        event.widget.config(highlightbackground=self.style_data.border_color, highlightcolor=self.style_data.border_color)
+
+    def reset(self, key, index):
+        """Reset la valeur d'un input."""
+        entry = self.entries[key]
+        entry.delete(0, tk.END)
+
+        data = self.get_data()
+        if data:
+            value = data[index]["value"]
+            entry.insert(0, value)
+
+    def hook_imagewidget(self):
+        # Récupère le widget parent et s'abonne à son événement
+        if self.master and hasattr(self.master, "winfo_children"):
+            for widget in self.master.winfo_children():
+                widget.bind("<<ImageUpdated>>", self.update_metadata)
+
+    def get_data(self):
+        image = self.image_data.pil_image
+        path = self.image_data.image_path
+
+        if image and path:
+            latitude, longitude = get_coordinates(image)
+            return [{"key": "nom", "value": get_name(path)},
+                    {"key": "format", "value": get_format(image)},
+                    {"key": "poids", "value": get_poids(path)},
+                    {"key": "dimensions", "value": get_size(image)},
+                    {"key": "date_creation", "value": get_date_taken(image)},
+                    {"key": "date_modification", "value": get_date_modify(path)},
+                    {"key": "latitude", "value": latitude},
+                    {"key": "longitude", "value": longitude},]
+        return None
+
+    def update_metadata(self, event=None):
+        data = self.get_data()
+        if data:
+            for i, label_data in enumerate(data):
+                key, value = label_data["key"], label_data["value"]
+                if value is None:
+                    value = ""
+                value = str(value)
+                if key in self.entries:
+                    entry = self.entries[key]
+                    entry.config(state="normal")
+                    entry.delete(0, tk.END)
+                    entry.insert(0, value)
+                    if self.labels[i]["disable"]:
+                        entry.config(state="disabled")
+
+        else:
+            for key in self.entries:
+                self.entries[key].delete(0, tk.END)
+
+def get_date_taken(image):
+    exif_data = piexif.load(image.info['exif'])
+    date_prise_vue = exif_data.get("0th", {}).get(piexif.ImageIFD.DateTime)
+
+    if date_prise_vue:
+        date_prise_vue = date_prise_vue.decode('utf-8')
+        date_obj = datetime.strptime(date_prise_vue, '%Y:%m:%d %H:%M:%S')
+        date_prise_vue_formattee = date_obj.strftime('%Y-%m-%d %H:%M:%S')
+        return date_prise_vue_formattee
+    else:
+        return None
+
+def get_date_modify(path):
+    date_modification_timestamp = os.path.getmtime(path)
+    date_modification = datetime.fromtimestamp(date_modification_timestamp)
+    return date_modification
+
+def get_poids(path):
+    poids_image = os.path.getsize(path)
+    poids_image_ko = (poids_image / 1024)
+    return f"{poids_image_ko:.2f} Ko"
+
+def get_name(path):
+    return ".".join(path.split("/")[-1].split(".")[:-1])
+
+def get_format(image):
+    return image.format
+
+def get_size(image):
+    return f"{image.width} x {image.height}"
+
+def get_geotagging(exif_data):
+    """Extrait les données GPS du dictionnaire EXIF"""
+    if "GPS" in exif_data:
+        gps_data = exif_data["GPS"]
+        latitude = gps_data.get(piexif.GPSIFD.GPSLatitude)
+        longitude = gps_data.get(piexif.GPSIFD.GPSLongitude)
+
+        if latitude and longitude:
+            lat_deg = latitude[0][0] / latitude[0][1]
+            lat_min = latitude[1][0] / latitude[1][1]
+            lat_sec = latitude[2][0] / latitude[2][1]
+            latitude = lat_deg + (lat_min / 60) + (lat_sec / 3600)
+
+            lon_deg = longitude[0][0] / longitude[0][1]
+            lon_min = longitude[1][0] / longitude[1][1]
+            lon_sec = longitude[2][0] / longitude[2][1]
+            longitude = lon_deg + (lon_min / 60) + (lon_sec / 3600)
+
+            # Si la latitude ou longitude sont dans l'hémisphère sud ou ouest, on inverse les signes
+            if gps_data.get(piexif.GPSIFD.GPSLatitudeRef).decode() == 'S':
+                latitude = -latitude
+            if gps_data.get(piexif.GPSIFD.GPSLongitudeRef).decode() == 'W':
+                longitude = -longitude
+
+            return latitude, longitude
+    return None, None
+
+def get_coordinates(image):
+    """Retourne les coordonnées GPS d'une image si disponibles"""
+    exif_data = piexif.load(image.info['exif'])
+
+    latitude, longitude = get_geotagging(exif_data)
+    if latitude is not None and longitude is not None:
+        return latitude, longitude
+    else:
+        return None, None
